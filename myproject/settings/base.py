f3b6e13121b4e48eaa9e338cabd143039b722270
kishoraditya <kishoradityasc@gmail.com>
2025-04-09 23:33:49 +0530

    cicd18

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -44,7 +44,6 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     "home",
-    "search",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.embeds",
