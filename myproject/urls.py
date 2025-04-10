697df7113f0483e7e1fac841c5b21fd5358f889e
kishoraditya <kishoradityasc@gmail.com>
2025-04-10 21:54:03 +0530

    search6

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,7 +6,7 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-from search.views import search as search_view
+from search.views import search as search_views
    
 
 urlpatterns = [
