24ab7d11d426aa16da17c8a9e8749a67d5bac872
kishoraditya <kishoradityasc@gmail.com>
2025-04-17 14:22:09 +0530

    cd10

--- IaC/terraform/modules/secrets/main.tf
+++ IaC/terraform/modules/secrets/main.tf
@@ -5,19 +5,21 @@ resource "random_string" "secret_suffix" {
 }
 
 resource "time_sleep" "wait_for_secret_replacement" {
-  depends_on = [aws_secretsmanager_secret.app_secrets]
   create_duration = "10s"
 }
 
 resource "aws_secretsmanager_secret" "app_secrets" {
   name        = "${var.environment}/${var.app_name}/secrets_"
   description = "Secrets for the ${var.app_name} application in ${var.environment} environment"
-  recovery_window_in_days = 0
-  depends_on = [time_sleep.wait_for_secret_replacement]
+  
   tags = {
     Environment = var.environment
     Application = var.app_name
   }
+  lifecycle {
+    create_before_destroy = true
+  }
+  depends_on = [time_sleep.wait_for_secret_slot]
 }
 
 resource "aws_secretsmanager_secret_version" "app_secrets" {
